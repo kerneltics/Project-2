b3f98f807fe05f82ff88856206272ea4940bf5ce
Alizelabd <lordlords834@gmail.com>
2024-02-20 20:40:05 +0300

    edite app

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -1,13 +1,30 @@
-// import { Icons } from "@/config/icons";
-// import { Container } from "@/components/Container";
-// import { Heading } from "@/components/Heading";
-// import { Section } from "@/components/Section";
-// import { SubHeading } from "@/components/SubHeading";
+import { AdminLayout } from "@/layout/AdminLayout";
+import { RootLayout } from "@/layout/RootLayout";
+import { AdminPage } from "@/pages/AdminPage";
+import { HomePage } from "@/pages/HomePage";
+import { ListingDetailsPage } from "@/pages/ListingDetailsPage";
+import { ListingsPage } from "@/pages/ListingsPage";
+import { LoginPage } from "@/pages/LoginPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="listings" element={<ListingsPage />} />
+          <Route path=":listingId" element={<ListingDetailsPage />} />
+        </Route>
+
+        <Route path="admin" element={<AdminLayout />}>
+          <Route index element={<AdminPage />} />
+        </Route>
+        <Route path="login" element={<AdminLayout />}>
+          <Route index element={<LoginPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
