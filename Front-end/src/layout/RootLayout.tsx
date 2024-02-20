912ecf17c0c63afe2a9a86a450f6202bba23e676
Alizelabd <lordlords834@gmail.com>
2024-02-20 21:36:29 +0300

    تعديل الكونتينر و  APP PAGE

--- Front-end/src/layout/RootLayout.tsx
+++ Front-end/src/layout/RootLayout.tsx
@@ -1,16 +1,16 @@
 import { Outlet } from "react-router-dom";
 
-import { Container } from "@/components/Container";
 import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
 
 export const RootLayout = () => {
   return (
-    <Container className="flex flex-col">
-      {/* Add header component below */}
+   <>
+      <Header />
       <main className="flex-1">
         <Outlet />
       </main>
       <Footer />
-    </Container>
+   </>
   );
 };
