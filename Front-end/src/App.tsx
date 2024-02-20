780f45a34d7f22c68eadf779e9e342d2c99f837d
Alizelabd <lordlords834@gmail.com>
2024-02-21 00:04:21 +0300

    format code only

--- Front-end/src/App.tsx
+++ Front-end/src/App.tsx
@@ -1,4 +1,4 @@
-import {  Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import { AdminLayout } from "@/layout/AdminLayout";
 import { RootLayout } from "@/layout/RootLayout";
