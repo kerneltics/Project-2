3e90ff12beff6343b5c7603148bec73da9c6137b
Ahmed Altariqi <ahmed.altariqi1@gmail.com>
2024-02-21 22:22:56 +0300

    fix image import path

--- Front-end/src/pages/ListingDetailsPage.tsx
+++ Front-end/src/pages/ListingDetailsPage.tsx
@@ -1,8 +1,8 @@
 // import { useParams } from "react-router-dom";
 // import { Icons } from "@/config/icons";
-import exImg from "@/../public/images/exmple.png";
-
 import { SubHeading } from "@/components/SubHeading";
+
+import exImg from "/images/exmple.png";
 
 export const ListingDetailsPage = () => {
   // const { listingId } = useParams();
