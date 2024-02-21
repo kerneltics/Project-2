4e1505ba47541a744b0ab199a794d93fe4472aff
Alizelabd <lordlords834@gmail.com>
2024-02-21 22:23:31 +0300

    import hero section in rootlayout page

--- Front-end/src/layout/RootLayout.tsx
+++ Front-end/src/layout/RootLayout.tsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom";
 
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
+import { HeroSection } from "@/components/HeroSection";
 
 export const RootLayout = () => {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
+      <HeroSection />
       <main className="container flex-1">
         <Outlet />
       </main>
