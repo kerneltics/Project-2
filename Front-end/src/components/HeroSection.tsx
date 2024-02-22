bc0b2f5184ab24ae3ecf307cc767007f07eb23ff
Alizelabd <lordlords834@gmail.com>
2024-02-22 23:10:01 +0300

    edite spacing in hero section

--- Front-end/src/components/HeroSection.tsx
+++ Front-end/src/components/HeroSection.tsx
@@ -7,17 +7,17 @@ import { SubHeading } from "@/components/SubHeading";
 
 export const HeroSection = () => {
   return (
-    <div className="h-[960px]">
-      <div className="bg-hero absolute inset-0 top-[71px] -z-10 h-[960px] bg-cover bg-bottom md:top-28" />
-      <Heading className="pt-[30px] text-center text-[16px] md:pt-[40px] md:text-[40px]">
+    <div className="h-[760px] md:h-[500px]">
+      <div className="bg-hero absolute inset-0 -z-10 h-[760px] bg-cover bg-center md:top-28  md:h-[500px]" />
+      <Heading className="mt-[80px] text-center text-[20px] md:mt-[40px] md:pt-[20px] md:text-[40px]">
         استثمر في مستقبلك مع أفضل عروض العقارات في السعودية والخليج!
       </Heading>
-      <SubHeading className="m-auto my-5 max-w-[50ch] text-center text-[15px] font-normal text-muted-foreground md:text-[30px]">
+      <SubHeading className="m-auto my-5 max-w-[50ch] text-center text-[18px] font-normal text-muted-foreground md:text-[30px]">
         المكان الأمثل لاكتشاف عقارات فريدة واستثنائية في منطقتنا. نحن نفتخر
         بتقديم خدمات عقارية متميزة تلبي احتياجات جميع عملائنا بكفاءة واحترافية.
       </SubHeading>
       <Link
-        className="m-auto block w-[150px] rounded-lg bg-primary py-4 text-center text-[14px] font-bold text-white"
+        className="m-auto block w-[130px] rounded-lg bg-primary py-3 text-center text-[16px] font-bold text-white"
         to="/"
       >
         تصفح العقارات
