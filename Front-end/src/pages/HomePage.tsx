c4c5286e676df7e451ea5ebe6d7aed860b116ad9
liix5 <layan14243@gmail.com>
2024-02-22 14:32:33 +0300

    added slider, fixed the font, added some icons

--- Front-end/src/pages/HomePage.tsx
+++ Front-end/src/pages/HomePage.tsx
@@ -1,9 +1,13 @@
 import ContactUs from "@/components/ContactUs.tsx";
 import { Section } from "@/components/Section";
+import { CustomerOpinions } from "@/components/customerOpinions";
 
 export const HomePage = () => {
   return (
     <div>
+      <Section>
+        <CustomerOpinions />
+      </Section>
       <Section>
         <ContactUs />
       </Section>
