9328a3a4d166d6bf0a5ef04c7b00fc1a0f39e625
Ahmed Altariqi <ahmed.altariqi1@gmail.com>
2024-02-24 23:09:55 +0300

    resolve conflict

--- Front-end/src/pages/HomePage.tsx
+++ Front-end/src/pages/HomePage.tsx
@@ -3,15 +3,10 @@ import ContactUs from "@/components/ContactUs.tsx";
 import { CustomerOpinions } from "@/components/customerOpinions";
 import { FeaturedListings } from "@/components/FeaturedListings";
 import { HeroSection } from "@/components/HeroSection";
-<<<<<<< HEAD
-=======
-import { Section } from "@/components/Section";
->>>>>>> 35827b79280b4ce13fac9ba1808c917cdc2b15f6
 import Services from "@/components/Servies";
 
 export const HomePage = () => {
   return (
-<<<<<<< HEAD
     <div className="mb-[100px] space-y-[100px]">
       <HeroSection />
       <About />
@@ -19,24 +14,6 @@ export const HomePage = () => {
       <FeaturedListings />
       <CustomerOpinions />
       <ContactUs />
-=======
-    <div>
-      <Section>
-        <HeroSection />
-      </Section>
-      <Section>
-        <About />
-      </Section>
-      <Section>
-        <Services />
-      </Section>
-      <Section>
-        <CustomerOpinions />
-      </Section>
-      <Section>
-        <ContactUs />
-      </Section>
->>>>>>> 35827b79280b4ce13fac9ba1808c917cdc2b15f6
     </div>
   );
 };
