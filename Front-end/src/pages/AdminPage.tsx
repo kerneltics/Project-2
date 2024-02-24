40c95415de63921eddc103ecf4b3ee846a046d2f
Khaled Almansour <Khaled@mugendai.io>
2024-02-24 22:56:29 +0300

    Connect Login Page to BackEnd and Design the Creating Product Form

--- Front-end/src/pages/AdminPage.tsx
+++ Front-end/src/pages/AdminPage.tsx
@@ -1,9 +1,11 @@
+import AddProduct from "@/components/AddProduct.tsx";
 import { ProtectedRoute } from "@/components/ProtectedRoute";
 
 export const AdminPage = () => {
   return (
     <ProtectedRoute>
       <h1>Admin page</h1>
+      <AddProduct />
     </ProtectedRoute>
   );
 };
