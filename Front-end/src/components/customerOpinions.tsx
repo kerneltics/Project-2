99fd7a7753d69b5fbbd0fe84594fea58f3cab912
Alizelabd <lordlords834@gmail.com>
2024-02-24 20:03:17 +0300

    format code

--- Front-end/src/components/customerOpinions.tsx
+++ Front-end/src/components/customerOpinions.tsx
@@ -1,68 +1,78 @@
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import React, { useState, useEffect, useRef } from "react";
+
+import React, { useEffect, useRef, useState } from "react";
+
+import customer1 from "@/../public/customerOpinionsImages/customers/customer1.png";
+import customer2 from "@/../public/customerOpinionsImages/customers/customer2.png";
+import customer3 from "@/../public/customerOpinionsImages/customers/customer3.png";
+import customer4 from "@/../public/customerOpinionsImages/customers/customer4.png";
+import customer5 from "@/../public/customerOpinionsImages/customers/customer5.png";
 import Slider from "react-slick";
+
 import { Icons } from "@/config/icons";
+
 import { Heading } from "./Heading";
 
-import customer1 from "@/../public/customerOpinionsImages/customers/customer1.png"
-import customer2 from "@/../public/customerOpinionsImages/customers/customer2.png" 
-import customer3 from "@/../public/customerOpinionsImages/customers/customer3.png"
-import customer4 from "@/../public/customerOpinionsImages/customers/customer4.png"
-import customer5 from "@/../public/customerOpinionsImages/customers/customer5.png"
-
-
 // fake data
-const CustomersData=[
+const CustomersData = [
   {
-    id:1,
+    id: 1,
     photo: customer1,
     rate: 5,
-    customerName:"عبدالله",
-    comment:'"كانت تجربتي مع اصول مذهلة بكل معنى الكلمة. لقد قدموا لي الدعم والمشورة في كل خطوة من خطوات عملية شراء العقار."'
+    customerName: "عبدالله",
+    comment:
+      '"كانت تجربتي مع اصول مذهلة بكل معنى الكلمة. لقد قدموا لي الدعم والمشورة في كل خطوة من خطوات عملية شراء العقار."',
   },
   {
-    id:2,
+    id: 2,
     photo: customer2,
     rate: 4,
-    customerName:"احمد",
-    comment: '"لا يمكنني سوى التأكيد على روعة تعامل اصول، كانوا مصدر ثقة ومساعدة في كل مرحلة من مراحل العملية."'
+    customerName: "احمد",
+    comment:
+      '"لا يمكنني سوى التأكيد على روعة تعامل اصول، كانوا مصدر ثقة ومساعدة في كل مرحلة من مراحل العملية."',
   },
   {
-    id:3,
+    id: 3,
     photo: customer3,
     rate: 5,
-    customerName:"حاتم",
-    comment: '"لا يمكنني سوى التعبير عن إعجابي بكفاءة واحترافية اصول مذهلة، كانت تجربة فريدة ومميزة في عالم العقارات."'
+    customerName: "حاتم",
+    comment:
+      '"لا يمكنني سوى التعبير عن إعجابي بكفاءة واحترافية اصول مذهلة، كانت تجربة فريدة ومميزة في عالم العقارات."',
   },
   {
-    id:4,
+    id: 4,
     photo: customer4,
     rate: 5,
-    customerName:"خالد",
-    comment: '"لا يمكنني سوى التعبير عن إعجابي بكفاءة واحترافية اصول، كانت تجربة فريدة ومميزة في عالم العقارات."'
+    customerName: "خالد",
+    comment:
+      '"لا يمكنني سوى التعبير عن إعجابي بكفاءة واحترافية اصول، كانت تجربة فريدة ومميزة في عالم العقارات."',
   },
   {
-    id:5,
+    id: 5,
     photo: customer5,
     rate: 3,
-    customerName:"سعد",
-    comment: '"كانت تجربتي مع اصول مذهلة جيدة بشكل عام، لكن كان هناك بعض اللحظات التي شعرت فيها ببعض الارتباك في التواصل والتنسيق."'
-  },{
-    id:1,
-    photo: customer1,
-    rate: 5,
-    customerName:"عبدالله",
-    comment:'"كانت تجربتي مع اصول مذهلة بكل معنى الكلمة. لقد قدموا لي الدعم والمشورة في كل خطوة من خطوات عملية شراء العقار."'
+    customerName: "سعد",
+    comment:
+      '"كانت تجربتي مع اصول مذهلة جيدة بشكل عام، لكن كان هناك بعض اللحظات التي شعرت فيها ببعض الارتباك في التواصل والتنسيق."',
   },
   {
-    id:2,
+    id: 1,
+    photo: customer1,
+    rate: 5,
+    customerName: "عبدالله",
+    comment:
+      '"كانت تجربتي مع اصول مذهلة بكل معنى الكلمة. لقد قدموا لي الدعم والمشورة في كل خطوة من خطوات عملية شراء العقار."',
+  },
+  {
+    id: 2,
     photo: customer2,
     rate: 4,
-    customerName:"احمد",
-    comment: '"لا يمكنني سوى التأكيد على روعة تعامل اصول، كانوا مصدر ثقة ومساعدة في كل مرحلة من مراحل العملية."'
+    customerName: "احمد",
+    comment:
+      '"لا يمكنني سوى التأكيد على روعة تعامل اصول، كانوا مصدر ثقة ومساعدة في كل مرحلة من مراحل العملية."',
   },
-]
+];
 
 // Define Props type for the Stars component
 type StarsProps = {
@@ -71,36 +81,44 @@ type StarsProps = {
 
 //* filled stars component
 const Stars: React.FC<StarsProps> = ({ count }) => (
-  <div className=" flex">{Array.from({ length: count }, (_, index) => <Icons.star key={index} />)}</div>
-); 
+  <div className=" flex">
+    {Array.from({ length: count }, (_, index) => (
+      <Icons.star key={index} />
+    ))}
+  </div>
+);
 
 //* empty stars component
 const EmptyStars: React.FC<StarsProps> = ({ count }) => (
-  <div className=" flex">{Array.from({ length: count }, (_, index) => <Icons.emptyStar key={index} />)}</div>
+  <div className=" flex">
+    {Array.from({ length: count }, (_, index) => (
+      <Icons.emptyStar key={index} />
+    ))}
+  </div>
 );
 
-
-const customers= CustomersData.map((c)=>{
-  return(
+const customers = CustomersData.map((c) => {
+  return (
     <div className=" text-center  ">
       <div className=" pb-6">
-        <h2 className=" font-medium py-2  text-2xl text-black">{c.customerName}</h2>
-        <div className=" flex  justify-center  items-center">
+        <h2 className=" py-2 text-2xl  font-medium text-black">
+          {c.customerName}
+        </h2>
+        <div className=" flex  items-center  justify-center">
           <Stars count={c.rate} />
-          <EmptyStars count={5-c.rate} />
+          <EmptyStars count={5 - c.rate} />
         </div>
       </div>
-      <div>
-        
-      </div>
-      
-      <p className={` before:content-[${<Icons.beforCotation/>}] text-[#3E3E3E] text-xl`}>
+      <div></div>
+
+      <p
+        className={` before:content-[${(<Icons.beforCotation />)}] text-xl text-[#3E3E3E]`}
+      >
         {c.comment}
       </p>
-      
     </div>
-  ) 
-})
+  );
+});
 
 export function CustomerOpinions() {
   const [nav1, setNav1] = useState<Slider | null>(null);
@@ -108,50 +126,60 @@ export function CustomerOpinions() {
   const sliderRef1 = useRef<Slider | null>(null);
   const sliderRef2 = useRef<Slider | null>(null);
 
-
   useEffect(() => {
     setNav1(sliderRef1.current);
     setNav2(sliderRef2.current);
   }, []);
 
-  
-
   return (
-    <section className=" w-full flex justify-center flex-col items-center gap-8">
-      <Heading className=" pb-16">
-        آراء العملاء
-      </Heading>
-    <div className=" w-4/5">
-      <Slider
-        asNavFor={nav1}
-        ref={(slider) => (sliderRef2.current = slider)}
-        slidesToShow={5}
-        focusOnSelect={true}
-        autoplay={true}
-        autoplaySpeed={3000}
-        arrow={true}
-      >
-        <div>
-         {/* moved it a little so every image align with its comment */}
-          <img src={customer1} className=" md:translate-x-8 lg:translate-x-11 xl:translate-x-20"></img>
-        </div>
-        <div>
-          <img src={customer2}  className=" md:translate-x-8 lg:translate-x-11  xl:translate-x-20"></img>
-        </div>
-        <div>
-          <img src={customer3} className="md:translate-x-8 lg:translate-x-11 xl:translate-x-20"></img>
-        </div>
-        <div>
-          <img src={customer4} className=" md:translate-x-8 lg:translate-x-11 xl:translate-x-20"></img>
-        </div>
-        <div>
-          <img src={customer5} className=" md:translate-x-8 lg:translate-x-11 xl:translate-x-20"></img>
-        </div>
-      </Slider>
-      <Slider asNavFor={nav2} ref={(slider) => (sliderRef1.current = slider)}>
-        {customers}
-      </Slider>
-    </div>
+    <section className=" flex w-full flex-col items-center justify-center gap-8">
+      <Heading className=" pb-16">آراء العملاء</Heading>
+      <div className=" w-4/5">
+        <Slider
+          asNavFor={nav1}
+          ref={(slider) => (sliderRef2.current = slider)}
+          slidesToShow={5}
+          focusOnSelect={true}
+          autoplay={true}
+          autoplaySpeed={3000}
+          arrow={true}
+        >
+          <div>
+            {/* moved it a little so every image align with its comment */}
+            <img
+              src={customer1}
+              className=" md:translate-x-8 lg:translate-x-11 xl:translate-x-20"
+            ></img>
+          </div>
+          <div>
+            <img
+              src={customer2}
+              className=" md:translate-x-8 lg:translate-x-11  xl:translate-x-20"
+            ></img>
+          </div>
+          <div>
+            <img
+              src={customer3}
+              className="md:translate-x-8 lg:translate-x-11 xl:translate-x-20"
+            ></img>
+          </div>
+          <div>
+            <img
+              src={customer4}
+              className=" md:translate-x-8 lg:translate-x-11 xl:translate-x-20"
+            ></img>
+          </div>
+          <div>
+            <img
+              src={customer5}
+              className=" md:translate-x-8 lg:translate-x-11 xl:translate-x-20"
+            ></img>
+          </div>
+        </Slider>
+        <Slider asNavFor={nav2} ref={(slider) => (sliderRef1.current = slider)}>
+          {customers}
+        </Slider>
+      </div>
     </section>
   );
 }
