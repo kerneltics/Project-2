b0803d829e622e3237c902741d92871ce0603bbc
Ghazi18 <suruji.188@gmail.com>
2024-02-25 02:05:59 +0300

    test

--- Front-end/src/pages/ListingDetailsPage.tsx
+++ Front-end/src/pages/ListingDetailsPage.tsx
@@ -1,10 +1,6 @@
 // import { useParams } from "react-router-dom";
-<<<<<<< HEAD
-import { OneReal } from "@/components/OneReal";
-=======
 // import { Icons } from "@/config/icons";
 import { SubHeading } from "@/components/SubHeading";
->>>>>>> cd0c64dbab3dbeefd9a67a3078868f1770a80e68
 
 import exImg from "/images/exmple.png";
 
