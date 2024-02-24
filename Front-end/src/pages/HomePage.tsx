c5146d1fc50d9120ed6a02ce7ccfe28e8a0a58e5
Alizelabd <lordlords834@gmail.com>
2024-02-24 22:51:21 +0300

    remove tajwal font from project and update to header

--- Front-end/src/pages/HomePage.tsx
+++ Front-end/src/pages/HomePage.tsx
@@ -1,20 +1,21 @@
+import About from "@/components/About";
 import ContactUs from "@/components/ContactUs.tsx";
 import { CustomerOpinions } from "@/components/customerOpinions";
 import { HeroSection } from "@/components/HeroSection";
 import { Section } from "@/components/Section";
-import About from "@/components/About";
 import Services from "@/components/Servies";
-
 
 export const HomePage = () => {
   return (
     <div>
       <Section>
         <HeroSection />
-<About/>
       </Section>
       <Section>
-                        <Services/>
+        <About />
+      </Section>
+      <Section>
+        <Services />
       </Section>
       <Section>
         <CustomerOpinions />
