16b379bbc39e5cc0e424fb8c04654834652c8050
aryam alharbi <aryamsultan45a@gmail.com>
2024-02-25 12:37:07 +0300

    screen compatible adjustment

--- Front-end/src/components/About.tsx
+++ Front-end/src/components/About.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
 
 const About = () => {
-  return (
-    <div className="bg-white p-5 rounded-md shadow-md mx-auto max-w-5xl">
-      <div className="flex items-center justify-center space-x-2">
-  <div className="w-60 border-t  mx-4 bg-gray-200"></div> 
-    <h2 className="text-073762 text-3xl mb-6 font-bold text-center">من نحن وماذا نقدم </h2>
-  <div className="w-60 border-t mx-4 bg-gray-200"></div> 
-</div>
-
-      <div className="mx-auto max-w-2xl">
-      
-        <p className="text-black text-md mt-8 leading-7 ">
-        في اصول، نمتلك فريقًا من الخبراء المتخصصين في مجال العقارات، الذين يجمعون بين الخبرة العميقة والشغف 
-        بتحقيق رؤى العملاء. نحن نفهم أن كل عميل فريد، ونسعى جاهدين لتقديم الخدمة المخصصة التي تلبي تمامًا
-         متطلباتهم وتطلعاتهم. هدفنا هو إرشادك وإيجاد العقار المناسب لك بأقل وقت وجهد ممكن.
-        </p>
-      </div>
-    </div>
-  );
-};
-
-export default About;
+                        return (
+                                                
+                                                <div className="bg-white p-5 rounded-md shadow-md mx-auto max-w-5xl">
+                                                <div className="flex flex-col md:flex-row items-center justify-center space-y-4 md:space-y-0 md:space-x-6 mb-8">
+                                                  <div className="hidden md:block w-full md:w-60 border-t bg-gray-200"></div>
+                                                  <h2 className="text-3xl md:text-3xl mb-6 font-bold text-center md:text-right">من نحن وماذا نقدم</h2>
+                                                  <div className="hidden md:block w-full md:w-60 border-t bg-gray-200"></div>
+                                                </div>
+                      
+                            <div className="mx-auto max-w-2xl">
+                              <p className="text-black text-md mt-8 leading-7">
+                                في اصول، نمتلك فريقًا من الخبراء المتخصصين في مجال العقارات، الذين يجمعون بين الخبرة العميقة والشغف بتحقيق رؤى العملاء. نحن نفهم أن كل عميل فريد، ونسعى جاهدين لتقديم الخدمة المخصصة التي تلبي تمامًا متطلباتهم وتطلعاتهم. هدفنا هو إرشادك وإيجاد العقار المناسب لك بأقل وقت وجهد ممكن.
+                              </p>
+                            </div>
+                          </div>
+                        );
+                      };
+                      
+                      export default About;
+                      
--- Front-end/src/components/Servies.tsx
+++ Front-end/src/components/Servies.tsx
@@ -3,25 +3,25 @@ import { Icons } from "@/config/icons";
 
 const Services = () => {
                         return (
-                          <div className="flex flex-col  items-center justify-center h-screen font-ibm-plex-sans-arabic" >
-                            <div className="flex items-center justify-center space-x-6 ">
-                        <div className="w-60 border-t  mx-4 bg-gray-200 shadow-md"></div> 
-                          <h2 className="text-073762 text-4xl mb-2 font-bold text-center ">خدماتنا </h2>
-                        <div className="w-60 border-t mx-4 bg-gray-200 shadow-md"></div> 
+                          <div className="flex flex-col  items-center justify-center h-screen font-ibm-plex-sans-arabic px-4 lg:px-8" >
+                            <div className="flex flex-col lg:flex-row items-center justify-center space-y-4 lg:space-y-0 lg:space-x-6 mb-8">
+                            <div className="hidden md:block w-full md:w-60 border-t bg-gray-200"></div>
+                                                  <h2 className="text-3xl md:text-3xl mb-6 font-bold text-center md:text-right">خـدمـاتـنـا </h2>
+                                                  <div className="hidden md:block w-full md:w-60 border-t bg-gray-200"></div>
                       </div>
                       
-                            <div className="flex gap-8 mt-8">
+                            <div className=" flex flex-col lg:flex-row gap-8 ">
                               
-                              <div className="w-80 bg-white shadow-md text-black  rounded-tl-[20px] rounded-tr-[20px] rounded-bl-[20px] rounded-br-[20px]  p-8 flex flex-col items-center text-center">
+                              <div className="w-full md:w-80 bg-white shadow-md text-black rounded-2xl p-4 md:p-8 flex flex-col items-center text-center">
                                 <div className="bg-blue-100 rounded-tl-[20px] rounded-tr-[20px] rounded-bl-[20px] rounded-br-[20px] p-2 mb-2">
                                <Icons.customerService  />
                                 </div>
-                                <h3 className="text-2xl font-bold  mt-8">إستشارات عقارية </h3>
-                                <p className="text-md mt-2 text-opacity-75">فريق الدعم جاهز لمساعدتك! اتصل الآن أو حجز استشارة مدفوعة لمدة 30 دقيقة.</p>
+                                <h3 className="text-xl md:text-2xl font-bold mt-4 md:mt-8">إستشارات عقارية </h3>
+                                <p className="text-sm md:text-md mt-2 text-opacity-75">فريق الدعم جاهز لمساعدتك! اتصل الآن أو حجز استشارة مدفوعة لمدة 30 دقيقة.</p>
                               </div>
                       
                               
-                              <div className="w-80 bg-white shadow-md text-black rounded-tl-[20px] rounded-tr-[20px] rounded-bl-[20px] rounded-br-[20px] p-8 flex flex-col items-center text-center">
+                              <div className="w-full  md:w-80 bg-white shadow-md text-black rounded-tl-[20px] rounded-tr-[20px] rounded-bl-[20px] rounded-br-[20px] p-8 flex flex-col items-center text-center">
                                 <div className="bg-blue-100 rounded-tl-[20px] rounded-tr-[20px] rounded-bl-[20px] rounded-br-[20px] p-2 mb-2">
                                 <Icons.manageRealEstate/>
                                 </div>
