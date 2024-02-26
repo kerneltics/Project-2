1f29af2864b4611c2793d2ade332f5a78e55deb8
Ahmed Altariqi <ahmed.altariqi1@gmail.com>
2024-02-26 09:59:04 +0300

    Set the query cache manually to avoid refetching the same listings

--- Front-end/src/hooks/use-featured-listings.ts
+++ Front-end/src/hooks/use-featured-listings.ts
@@ -1,14 +1,23 @@
 import { listingsSchema } from "@/schema";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 
 import { api } from "@/lib/api";
 
 const ONE_DAY_IN_MILLISECONDS = 60 * 60 * 24 * 1000;
 
 export const useFeaturedListings = () => {
+  const queryClient = useQueryClient();
   return useQuery({
     queryKey: ["featured-listings"],
-    queryFn: fetchFeaturedListings,
+    queryFn: async () => {
+      const listings = await fetchFeaturedListings();
+
+      for (const listing of listings) {
+        queryClient.setQueryData([`listing`, listing.id.toString()], listing);
+      }
+
+      return listings;
+    },
     staleTime: ONE_DAY_IN_MILLISECONDS,
     retry: false,
   });
