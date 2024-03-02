5b0d87608a98a732480e22a33fb63553d08a79dc
Khaled Almansour <Khaled@mugendai.io>
2024-03-02 18:51:52 +0300

    small fixes

--- Front-end/src/components/HeroSection.tsx
+++ Front-end/src/components/HeroSection.tsx
@@ -4,7 +4,7 @@ export const HeroSection = () => {
   return (
     <div className="h-[100dvh] bg-hero bg-cover bg-bottom bg-no-repeat">
       <div className="container pt-8">
-        <Heading className="text-center text-[20px] md:text-[40px]">
+        <Heading className="mt-24 text-center text-[20px] md:text-[40px]">
           استثمر في مستقبلك مع أفضل عروض العقارات في السعودية والخليج!
         </Heading>
         <p className="m-auto my-10 max-w-[50ch] text-center text-[18px] font-normal text-muted-foreground md:text-[30px]">
